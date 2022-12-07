17616f15a32577a194464b8d58d2f96098e36333
sh3lbsd <86627394+sh3lbsd@users.noreply.github.com>
2022-12-07 17:38:05 -0600

    Social Icons

--- src/subComponents/LogoComponent.js
+++ src/subComponents/LogoComponent.js
@@ -2,11 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 
-const Logo = styled;
+const Logo = styled.h1`
+display: inline-block;
+color: ${props => props.theme.text};
+font-family: 'Pacifico', cursive;
+position: fixed;
+left: 2rem;
+top: 2rem;
+z-index: 3;
+
+
+`
+
 const LogoComponent = () => {
     return (
         <Logo>
-{/* need emerie to make a little logo for her intials */}
+{/* need emerie to make a little logo for her initials */} EM
         </Logo>
     )
 }
---
+++ src/subComponents/SocialIcons.js
@@ -0,0 +1,50 @@
+import React from "react";
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+import { Facebook, Github, Twitter, YouTube } from "../components/AllSvgs";
+
+const Icons = styled.div`
+display: flex;
+flex-direction: column;
+align-items: center;
+
+position: fixed;
+bottom: 0;
+left; 2rem;
+
+z-index: 3;
+
+& > *:not(:last-child) {
+    margin: 0.5rem 0;
+  }
+`;
+
+const Line = styled.span`
+  width: 2px;
+  height: 8rem;
+  background-color: ${(props) => props.theme.text};
+`;
+
+const SocialIcons = () => {
+  return (
+    <Icons>
+      <div>
+        <NavLink style={{color: 'inherit'}} to="/">
+          <Github width={25} height={25} fill="currentColor" />
+        </NavLink>
+        <NavLink style={{color: 'inherit'}} to="/">
+          <Twitter width={25} height={25} fill="currentColor" />
+        </NavLink>
+        <NavLink style={{color: 'inherit'}} to="/">
+          <Facebook width={25} height={25} fill="currentColor" />
+        </NavLink>
+        <NavLink style={{color: 'inherit'}} to="/">
+          <YouTube width={25} height={25} fill="currentColor" />
+        </NavLink>
+      </div>
+      <Line />
+    </Icons>
+  );
+};
+
+export default SocialIcons;
